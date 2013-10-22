84f36d8b16859bfee54ada3d4142da9153aeb401
Mario Gómez <mario.gomez@wfp.org>
2013-10-22 16:20:58 -0600

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GRASPOfflineProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to GRASP Offline processor!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Use this tool to manually process XML files from GRASP.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose GRASP directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(178, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 145);
+            this.textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 56);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "About";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(295, 55);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data files";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(13, 136);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(295, 170);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 323);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "GRASP Ofline data processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections;
+using System.Xml;
+
+namespace GRASPOfflineProcessor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void message(String txt)
+        {
+            textBox1.Text = txt + "\r\n" + textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            message("Choose a GRASP directory and then click on \"Start\".");
+
+        }
+
+        private bool isAGRASPDir()
+        {
+            bool validStructure = true;
+
+            // Check conditions
+            if(!Directory.Exists(folderBrowserDialog1.SelectedPath+"\\database")) {
+                validStructure &= false;
+                if (!File.Exists(folderBrowserDialog1.SelectedPath + "\\database\\message.db"))
+                {
+                    validStructure &= false;
+                }
+                if (!File.Exists(folderBrowserDialog1.SelectedPath + "\\database\\forms.db"))
+                {
+                    validStructure &= false;
+                }
+            }
+            if (!Directory.Exists(folderBrowserDialog1.SelectedPath + "\\forms"))
+            {
+                validStructure &= false;
+            }
+            if (!Directory.Exists(folderBrowserDialog1.SelectedPath + "\\instances"))
+            {
+                validStructure &= false;
+            }
+            if (!Directory.Exists(folderBrowserDialog1.SelectedPath + "\\metadata"))
+            {
+                validStructure &= false;
+            }
+            if (Directory.Exists(folderBrowserDialog1.SelectedPath + "\\metadata"))
+            {
+                if (!File.Exists(folderBrowserDialog1.SelectedPath + "\\metadata\\message.db"))
+                {
+                    validStructure &= false;
+                }
+                if (!File.Exists(folderBrowserDialog1.SelectedPath + "\\metadata\\forms.db"))
+                {
+                    validStructure &= false;
+                }
+            }
+            return validStructure;
+        }
+
+        private void processForms()
+        {
+            SQLiteCommand command;
+            SQLiteDataReader reader;
+            String str;
+            string[] path;
+            XmlDocument doc = new XmlDocument();
+            if (!Directory.Exists(folderBrowserDialog1.SelectedPath + "\\output\\"))
+            {
+                Directory.CreateDirectory(folderBrowserDialog1.SelectedPath + "\\output\\");
+            }
+            FileStream f;
+            StreamWriter sw;
+
+            XmlNodeList enumId;
+            XmlNodeList sample_id;
+            XmlNodeList sample_name;
+
+            XmlDocument spec = new XmlDocument();
+            XmlNodeList spec_data;
+
+            XmlNodeList valuenode;
+
+            List<string> rosters = new List<string>();
+            foreach (string data in currentForms)
+            {
+                
+                f = File.Create(folderBrowserDialog1.SelectedPath + "\\output\\" + data + ".csv");
+                sw = new StreamWriter(f);
+
+                command = new SQLiteCommand("SELECT instanceFilePath FROM forms where displayName='" +
+                    data + "' AND (status='completed' OR status='submitted')",conn);
+                reader = command.ExecuteReader();
+
+
+                // LOAD File Header:
+                spec.Load(folderBrowserDialog1.SelectedPath + "\\forms\\" + data + ".xml");
+
+                spec_data = spec.GetElementsByTagName("data");
+                
+
+                sw.Write("formId,");
+                foreach (XmlNode datanode in spec_data[0].ChildNodes)
+                {
+                    if (datanode.ChildNodes.Count == 0)
+                    {
+                        sw.Write(datanode.Name + ",");
+                    }
+                    else if (datanode.ChildNodes.Count > 1) // ROSTER
+                    {
+                        // Create datafile for roaster:
+                        FileStream roster_file = File.Create(folderBrowserDialog1.SelectedPath + "\\output\\" + data + "_" + datanode.Name + ".csv");
+                        StreamWriter r_writer = new StreamWriter(roster_file);
+                        r_writer.Write("formId,");
+                        foreach (XmlNode subNode in datanode.ChildNodes)
+                        {
+                            r_writer.Write(subNode.Name+",");
+                        }
+                        r_writer.Write("\r\n");
+                        r_writer.Close();
+                        roster_file.Close();
+                    }
+                }
+                sw.Write("\r\n");
+
+                while (reader.Read())
+                {
+                    str = reader["instanceFilePath"].ToString();
+                    path = str.Split("/".ToCharArray());
+
+                    doc.Load(folderBrowserDialog1.SelectedPath + "\\instances\\" + path[path.Length - 2] + "\\" + path[path.Length - 1]);
+
+
+                    sw.Write(path[path.Length - 1] + ",");
+                    foreach (XmlNode datanode in spec_data[0].ChildNodes)
+                    {
+                        if (datanode.ChildNodes.Count == 0)
+                        {
+                            valuenode = doc.GetElementsByTagName(datanode.Name);
+                            if (valuenode.Count > 0)
+                            {
+                                sw.Write(valuenode[0].InnerText.Replace(',', ' ').Replace('\r', ' ').Replace('\n', ' ') + ",");
+                            }
+                            else
+                            {
+                                sw.Write("--,");
+                            }
+                        }
+                        else if (datanode.ChildNodes.Count > 1) // ROSTER
+                        {
+                            // Create datafile for roaster:
+                            FileStream roster_file = File.Open(folderBrowserDialog1.SelectedPath + "\\output\\" + data + "_" + datanode.Name + ".csv", FileMode.Append, FileAccess.Write);
+                            StreamWriter r_writer = new StreamWriter(roster_file);
+                            
+
+                            XmlNodeList rosterData = doc.GetElementsByTagName(datanode.Name);
+
+                            foreach (XmlNode node in rosterData)
+                            {
+                                XmlDocument newDoc = new XmlDocument();
+
+                                newDoc.LoadXml("<"+node.Name+">"+node.InnerXml+"</"+node.Name+">");
+                
+
+                                r_writer.Write(path[path.Length - 1] + ",");
+                                foreach (XmlNode subNode in datanode.ChildNodes)
+                                {
+                                    valuenode = newDoc.GetElementsByTagName(subNode.Name);
+                                    if (valuenode.Count > 0)
+                                    {
+                                        r_writer.Write(valuenode[0].InnerText.Replace(',', ' ').Replace('\r', ' ').Replace('\n', ' ') + ",");
+                                    }
+                                    else
+                                    {
+                                        r_writer.Write("--,");
+                                    }
+                                }
+                                r_writer.Write("\r\n");
+                            }
+                            r_writer.Flush();
+                            roster_file.Close();
+                        }
+                    }
+                    sw.Write("\r\n");
+                    message(path[path.Length - 1] + " written.");
+                }
+
+
+                sw.Flush();
+                reader.Close();
+                f.Close();
+            }
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (isAGRASPDir())
+                {
+                    message("Notice: GRASP Directory detected.\r\nCurrent working directory: " + folderBrowserDialog1.SelectedPath);
+                    message("Click \"start\" to process the questionnaires.");
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    message("Error: The selected directory isn't a valid GRASP directory.");
+                }
+            }
+            else
+            {
+                message("Notice: You must select a directory.");
+            }
+        }
+
+        SQLiteConnection conn = null;
+        List<string> currentForms;
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentForms = new List<string>();
+
+            conn = new SQLiteConnection("Data Source="+folderBrowserDialog1.SelectedPath + "\\metadata\\forms.db");
+
+            conn.Open();
+
+            SQLiteCommand listContents = new SQLiteCommand(@"
+SELECT status,count(*) as total FROM forms GROUP BY status
+            ", conn);
+
+            SQLiteDataReader reader = listContents.ExecuteReader();
+
+            string count_msg = "";
+            if (reader.HasRows)
+            {
+                count_msg += "Questionnaires found:\r\n";
+            }
+            else
+            {
+                count_msg += "Error: Metadata is empty, cannot continue.";
+                button2.Enabled = false;
+                return;
+            }
+            while (reader.Read())
+            {
+                count_msg += reader["status"].ToString() + ":" + reader["total"] + "\r\n";
+            }
+            message(count_msg);
+
+            reader.Close();
+
+            SQLiteCommand listForms = new SQLiteCommand(@"
+SELECT displayName FROM forms where status='new'
+            ", conn);
+
+            reader = listForms.ExecuteReader();
+            string forms_msg = "Forms found: \r\n";
+            while (reader.Read())
+            {
+               forms_msg += reader["displayName"].ToString()+"\r\n";
+               currentForms.Add(reader["displayName"].ToString()); // Adds forms to process.
+            }
+            message(forms_msg);
+
+            processForms();
+
+
+
+            button2.Enabled = false;
+        }
+
+    }
+}
